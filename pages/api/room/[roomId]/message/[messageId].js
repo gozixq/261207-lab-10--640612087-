bac5786c85757b9816d5b80a01960278fa47c101
gozixq <natsinee2545@gmail.com>
2022-10-02 13:58:19 +0700

    fff

--- pages/api/room/[roomId]/message/[messageId].js
+++ pages/api/room/[roomId]/message/[messageId].js
@@ -2,6 +2,24 @@ import { writeDB, readDB } from "../../../../../backendLibs/dbLib";
 
 export default function roomIdMessageIdRoute(req, res) {
   //read value from URL
-  const roomId = req.query.roomId;
-  const messageId = req.query.messageId;
+  if (req.method === "DELETE") {
+    const rooms = readDB();
+    const rId = req.query.roomId;
+    const roomIdx = rooms.findIndex((x) => x.roomId === rId);
+    if (roomIdx === -1) {
+      return res.status(404).json({ ok: false, message: "Invalid room id" });
+    }
+    const mId = req.query.messageId;
+    const messageIdx = rooms[roomIdx].messages.findIndex(
+      (x) => x.messageId === mId
+    );
+    if (messageIdx === -1) {
+      return res.status(404).json({ ok: false, message: "Invalid messege id" });
+    }
+
+    rooms[roomIdx].messages.splice(messageIdx, 1);
+    writeDB(rooms);
+
+    return res.json({ ok: true });
+  }
 }
